f00e61535fc6651804893ac5e160303f46551f0a
Squappi <69323875+Squappi@users.noreply.github.com>
2021-01-30 18:58:41 +0300

    Переделал

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 function getExchange(min, max) {
+  if (min < 0 || max < 0) return 'Invalid parameter';
   if (min === max) {
     return min;
   }
   if (min < max) {
-    return max + Math.random() * (min + 1 - max);
+    return max + Math.random() * (min - max);
   }
-  return min + Math.random() * (max + 1 - min);
+  return min + Math.random() * (max - min);
 }
 
 function getRandom(min, max) {
   return Math.round(getExchange(min, max));
 }
 
-function randomFloat(min, max) {
-  return Math.fround(getExchange(min, max)).toFixed(2);
+function randomFloat(min, max, e = 2) {
+  return Math.fround(getExchange(min, max)).toFixed(e);
 }
 
 alert(getRandom(0, 200));
